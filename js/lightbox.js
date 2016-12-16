d9446dab8d3cacd024102a0d764fa46b61e871a2
emilycopp <emily.copp@bell.net>
2016-12-16 17:25:12 -0500

    lightbox

---
+++ js/lightbox.js
@@ -0,0 +1,17 @@
+var $container = $('.container');
+var $lbImg = $('.lightbox-img');
+var $lb = $('.lightbox');
+var $btnClose = $('.btn-close');
+
+$container.on('touchstart click', 'a', function (e) {
+  var big = $(this).attr('href');
+  $lbImg.attr('src', big);
+  $lb.attr('data-state', 'visible');
+  $('html body').addClass('no-scroll');
+  e.preventDefault();
+});
+
+$btnClose.on('touchstart click', function () {
+  $lb.attr('data-state', 'hidden');
+  $('html body').removeClass('no-scroll');
+})
