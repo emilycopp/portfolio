d0fd012b812fc4cff7a6e554ae2ebf01c31d2acb
emilycopp <emily.copp@bell.net>
2016-12-16 17:42:14 -0500

    help

--- js/lightbox.js
+++ js/lightbox.js
@@ -7,7 +7,8 @@ var $btnClose = $('.btn-close');
 
 $container.on('touchstart click', 'a', function (e) {
   var big = $(this).attr('href');
-  $lbImg.attr('src', '{{site.baseurl}}' + big);
+  alert(big);
+  $lbImg.attr('src', big);
   $lb.attr('data-state', 'visible');
   $('html body').addClass('no-scroll');
   e.preventDefault();
